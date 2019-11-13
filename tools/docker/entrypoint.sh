49d58c3283b86192b4ac4c5eabe8a0c5327cfe0f
onelife <onelife.real@gmail.com>
2019-11-13 11:32:43 +0800

    Take out "freertos" component from esp-idf
    - Remove "coroutine" related code

--- tools/docker/entrypoint.sh
+++ tools/docker/entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
 
+if [ -d "$IDF_PATH/components/freertos" ]
+then
+    echo -e "* RTOS already exists\n"
+else
+    if [ -d "$RTT_PATH/rtos" ]
+    then
+        ln -s $RTT_PATH/rtos $IDF_PATH/components/freertos
+        echo -e "* RTOS ready\n"
+    else
+        echo -e "* RTOS does't exist\n"
+    fi
+fi
+
 . $IDF_PATH/export.sh
 
 exec "$@"
